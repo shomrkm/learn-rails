ffe41cec8a0e737df26555f8a9cf4d18a08ee22c
shomrkm <shochan1985@gmail.com>
2023-07-27 22:42:29 +0900

    feat: add test codes.

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ FROM ruby:3.1
 RUN apt update -qq && apt install -y postgresql-client
 RUN mkdir /myapp
 WORKDIR /myapp
-COPY Gemfile /myapp/Gemfile
-COPY Gemfile.lock /myapp/Gemfile.lock
-RUN bundle install
+# COPY Gemfile /myapp/Gemfile
+# COPY Gemfile.lock /myapp/Gemfile.lock
+# RUN bundle install
 COPY . /myapp
 
 COPY entrypoint.sh /usr/bin/
@@ -13,4 +13,4 @@ RUN chmod +x /usr/bin/entrypoint.sh
 ENTRYPOINT ["entrypoint.sh"]
 EXPOSE 3000
 
-CMD ["rails", "server", "-b", "0.0.0.0"]
+CMD ["sh"]
---
+++ book_admin/app/models/book.rb
@@ -0,0 +1,17 @@
+class Book < ApplicationRecord
+    scope :costly, -> { where("price > ?", 3000)}
+    scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+
+    belongs_to :publisher
+    has_many :book_authors
+    has_many :authors, through: :book_authors
+
+    validates :name, presence: true
+    validates :name, length: { maximum: 25 }
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validate do |book| 
+        if book.name.include?("exercise")
+            book.errors[:name] << "I dont like exercise."
+        end
+    end
+end
