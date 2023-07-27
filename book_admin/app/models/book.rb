1e5af6093ae0c0965c89bd3de7c056a86ff8b23d
shomrkm <shochan1985@gmail.com>
2023-07-28 08:39:13 +0900

    feat: add test codes.

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
+    enum sales_status: {
+        reservation: 0,  # 予約受付
+        now_on_sale: 1,  # 発売中
+        end_of_print: 2, # 販売終了
+    }
+
     scope :costly, -> { where("price > ?", 3000)}
     scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
 
@@ -11,7 +17,21 @@ class Book < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 0 }
     validate do |book| 
         if book.name.include?("exercise")
-            book.errors[:name] << "I dont like exercise."
+            book.errors.add(:name, "I dont like exercise.")
         end
+    end
+
+    before_validation do
+        self.name = self.name.gsub(/Cat/) do |matched|
+            "lovely #{matched}"
+        end
+    end
+
+    after_destroy :if => :high_price? do
+        Rails.logger.info "Book is deleted: #{self.attributes}"
+    end
+
+    def high_price?
+        price >= 5000
     end
 end
--- book_admin/db/schema.rb
+++ book_admin/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_230143) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_27_232457) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "penname"
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_230143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "publisher_id", null: false
+    t.integer "sales_status"
     t.index ["publisher_id"], name: "index_books_on_publisher_id"
   end
 
