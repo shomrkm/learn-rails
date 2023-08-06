c1680eb2475b2e53a0908dc4870326fd3276a5ea
shomrkm <shochan1985@gmail.com>
2023-08-06 01:11:14 +0000

    feat: add test codes.

--- active_job_example/app/jobs/async_log_job.rb
+++ active_job_example/app/jobs/async_log_job.rb
@@ -1,5 +1,9 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :default
+  queue_as :async_log
+  # ジョブのリトライ設定　
+  retry_on StandardError, wait: 5.seconds, attempts: 3
+  # ジョブの破棄設定
+  discard_on StandardError
 
   def perform(message: "hello")
     AsyncLog.create!(message: message)   # DB に保存する
